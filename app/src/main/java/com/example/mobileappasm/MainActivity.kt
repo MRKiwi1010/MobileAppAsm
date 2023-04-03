64ca69e54a49d822fb0378a2a41eef9b8dc922e2
Jason <leeaunliang@gmail.com>
2023-04-03 16:47:02 +0800

    zazd

--- app/src/main/java/com/example/mobileappasm/MainActivity.kt
+++ app/src/main/java/com/example/mobileappasm/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.mobileappasm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.testing1.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
 
 
