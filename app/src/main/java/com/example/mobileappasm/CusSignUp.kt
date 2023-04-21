41fddc2a8b6e0020d15c85deb964ad8d15473d0e
Jason <leeaunliang@gmail.com>
2023-04-21 21:23:04 +0800

    updates21/4 2.00pm

--- app/src/main/java/com/example/mobileappasm/CusSignUp.kt
+++ app/src/main/java/com/example/mobileappasm/CusSignUp.kt
@@ -1,11 +1,8 @@
 package com.example.mobileappasm
 
-import com.example.mobileappasm.R
-import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.service.autofill.Validators.or
 import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
@@ -15,12 +12,9 @@ import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
-import androidx.core.content.ContentProviderCompat.requireContext
-import androidx.core.content.ContextCompat.startActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
-import com.example.mobileappasm.ui.login.CusLoginPage
 import com.google.firebase.database.*
 
 class CusSignUp : Fragment() {
