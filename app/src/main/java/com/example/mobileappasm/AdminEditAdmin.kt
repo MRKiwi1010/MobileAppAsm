6574695322e37f32dd9071d75a16322dcfa44759
Ang Kang Wei <angkangwei4402@gmail.com>
2023-05-04 04:49:17 +0800

    newFile

--- app/src/main/java/com/example/mobileappasm/AdminEditAdmin.kt
+++ app/src/main/java/com/example/mobileappasm/AdminEditAdmin.kt
@@ -1,59 +1,134 @@
 package com.example.mobileappasm
-
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AdminEditAdmin.newInstance] factory method to
- * create an instance of this fragment.
- */
-class AdminEditAdmin : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_edit_admin, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AdminEditAdmin.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AdminEditAdmin().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
-}
+//
+//import android.app.Activity.RESULT_OK
+//import android.content.Intent
+//import android.net.Uri
+//import android.os.Bundle
+//import android.provider.MediaStore
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import android.widget.Toast
+//import androidx.fragment.app.Fragment
+//import com.bumptech.glide.Glide
+//import com.example.mobileappasm.databinding.FragmentAdminEditAdminBinding
+//import com.google.firebase.database.*
+//
+//private const val ARG_PARAM1 = "param1"
+//private const val ARG_PARAM2 = "param2"
+//
+//class AdminEditAdmin : Fragment() {
+//    private var param1: String? = null
+//    private var param2: String? = null
+//    private lateinit var binding: FragmentAdminEditAdminBinding
+//    private lateinit var firebaseDatabase: FirebaseDatabase
+//    private lateinit var adminUsername: String
+//    private var selectedImageUri: Uri? = null
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        arguments?.let {
+//            param1 = it.getString(ARG_PARAM1)
+//            param2 = it.getString(ARG_PARAM2)
+//        }
+//    }
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        binding = FragmentAdminEditAdminBinding.inflate(layoutInflater)
+//        return binding.root
+//    }
+//
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//        firebaseDatabase = FirebaseDatabase.getInstance()
+//        adminUsername = arguments?.getString("username") ?: ""
+//        fetchCustomerData()
+//        binding.btnSelectImage.setOnClickListener {
+//            selectImage()
+//        }
+//        binding.btnSaveChanges.setOnClickListener {
+//            saveChanges()
+//        }
+//    }
+//
+//    private fun fetchCustomerData() {
+//        val databaseReference = firebaseDatabase.getReference("admin")
+//        val query = databaseReference.orderByChild("username").equalTo(customerUsername)
+//        query.addValueEventListener(object : ValueEventListener {
+//            override fun onDataChange(dataSnapshot: DataSnapshot) {
+//                if (dataSnapshot.exists()) {
+//                    val customer = dataSnapshot.children.first().getValue(Customer::class.java)
+//                    binding.adminName.setText(customer?.name)
+//                    binding.adminAge.setText(customer?.age.toString())
+//                    binding.adminEmail.setText(customer?.email)
+//                    binding.adminUsername.setText(customer?.username)
+//                    binding.adminPassword.setText(customer?.password)
+//                    binding.adminContact.setText(customer?.contact)
+//                    when (customer?.gender) {
+//                        "Male" -> binding.maleBtn.isChecked = true
+//                        "Female" -> binding.femaleBtn.isChecked = true
+//                        "Other" -> binding.otherRadioButton.isChecked = true
+//                    }
+//                    Glide.with(requireContext())
+//                        .load(customer?.userimg)
+//                        .into(binding.adminImageView)
+//                }
+//            }
+//
+//            override fun onCancelled(databaseError: DatabaseError) {
+//                // Handle database error if needed
+//            }
+//        })
+//    }
+//
+//    private fun selectImage() {
+//        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+//        startActivityForResult(intent, 1000)
+//    }
+//
+//    private fun saveChanges() {
+//        val databaseReference = firebaseDatabase.getReference("users")
+//        val query = databaseReference.orderByChild("username").equalTo(customerUsername)
+//        query.addListenerForSingleValueEvent(object : ValueEventListener {
+//            override fun onDataChange(dataSnapshot: DataSnapshot) {
+//                if (dataSnapshot.exists()) {
+//                    val customer = dataSnapshot.children.first().getValue(Customer::class.java)
+//                    customer?.name = binding.adminName.text.toString()
+//                    customer?.age = binding.adminAge.text.toString().toInt()
+//                    customer?.email = binding.adminEmail.text.toString()
+//                    customer?.username = binding.adminUsername.text.toString()
+//                    customer?.password = binding.adminPassword.text.toString()
+//                    customer?.contact = binding.adminContact.text.toString()
+//                    when (binding.adminGender.checkedRadioButtonId) {
+//                        binding.maleBtn.id -> customer?.gender = "Male"
+//                        binding.femaleBtn.id -> customer?.gender = "Female"
+//                        binding.otherRadioButton.id -> customer?.gender = "Other"
+//                    }
+//                    if (selectedImageUri != null) {
+//                        uploadImage(customer!!)
+//                    } else {
+//                        saveCustomerdata(customer!!)
+//                    }
+//                }
+//            }
+//
+//            override fun onCancelled(databaseError: DatabaseError) {
+//                // Handle database error if needed
+//            }
+//        })
+//    }
+//
+//    companion object {
+//        @JvmStatic
+//        fun newInstance(param1: String, param2: String) =
+//            AdminEditCus().apply {
+//                arguments = Bundle().apply {
+//                    putString(ARG_PARAM1, param1)
+//                    putString(ARG_PARAM2, param2)
+//                }
+//            }
+//    }
+//}
--- app/src/main/java/com/example/mobileappasm/AdminEditCus.kt
+++ app/src/main/java/com/example/mobileappasm/AdminEditCus.kt
@@ -1,59 +1,157 @@
 package com.example.mobileappasm
 
+import android.app.Activity.RESULT_OK
+import android.app.ProgressDialog
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.example.mobileappasm.databinding.FragmentAdminEditCusBinding
+import com.google.firebase.database.*
+import com.google.firebase.storage.FirebaseStorage
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AdminEditCus.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AdminEditCus : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentAdminEditCusBinding
+    private lateinit var firebaseDatabase: FirebaseDatabase
+    private lateinit var customerUsername: String
+    private var selectedImageUri: Uri? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_edit_cus, container, false)
+    ): View {
+        binding = FragmentAdminEditCusBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        firebaseDatabase = FirebaseDatabase.getInstance()
+        customerUsername = arguments?.getString("customerUsername") ?: ""
+        fetchCustomerData()
+        binding.btnSelectImage.setOnClickListener {
+            selectImage()
+        }
+        binding.btnSaveChanges.setOnClickListener {
+            saveChanges()
+        }
+    }
+
+    private fun fetchCustomerData() {
+        val databaseReference = firebaseDatabase.getReference("users")
+        val query = databaseReference.orderByChild("username").equalTo(customerUsername)
+        query.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    val customer = dataSnapshot.children.first().getValue(Customer::class.java)
+                    binding.cusName.setText(customer?.name)
+                    binding.cusEmail.setText(customer?.email)
+                    binding.cusUsername.setText(customer?.username)
+                    binding.cusPassword.setText(customer?.password)
+                    Glide.with(requireContext()).load(customer?.imageUrl).into(binding.cusImageView)
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Toast.makeText(context, databaseError.message, Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    private fun selectImage() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, IMAGE_PICK_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == IMAGE_PICK_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
+            selectedImageUri = data.data
+            Glide.with(requireContext()).load(selectedImageUri).into(binding.cusImageView)
+        }
+    }
+
+    private fun saveChanges() {
+        val databaseReference = firebaseDatabase.getReference("users")
+        val query = databaseReference.orderByChild("username").equalTo(customerUsername)
+        query.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    dataSnapshot.children.first().ref.child("name")
+                        .setValue(binding.cusName.text.toString())
+                    dataSnapshot.children.first().ref.child("email")
+                        .setValue(binding.cusEmail.text.toString())
+                    dataSnapshot.children.first().ref.child("username")
+                        .setValue(binding.cusUsername.text.toString())
+                    dataSnapshot.children.first().ref.child("password")
+                        .setValue(binding.cusPassword.text.toString())
+                    uploadImageToFirebaseStorage()
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Toast.makeText(context, databaseError.message, Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    private fun uploadImageToFirebaseStorage() {
+        if (selectedImageUri == null) {
+            saveChangesToDatabase()
+            return
+        }
+        val progressDialog = ProgressDialog(requireContext())
+        progressDialog.setTitle("Uploading Image...")
+        progressDialog.show()
+
+        // Get a reference to the Firebase Storage and create a new file in the "images" directory
+        val storageReference =
+            FirebaseStorage.getInstance().getReference("images/${customerUsername}.jpg")
+        storageReference.putFile(selectedImageUri!!)
+            .addOnSuccessListener {
+                // If the image upload is successful, get the download URL and save it to the database
+                storageReference.downloadUrl.addOnSuccessListener { uri ->
+                    val databaseReference = firebaseDatabase.getReference("users")
+                    val query = databaseReference.orderByChild("username").equalTo(customerUsername)
+                    query.addListenerForSingleValueEvent(object : ValueEventListener {
+                        override fun onDataChange(dataSnapshot: DataSnapshot) {
+                            if (dataSnapshot.exists()) {
+                                dataSnapshot.children.first().ref.child("imageUrl")
+                                    .setValue(uri.toString())
+                                saveChangesToDatabase()
+                            }
+                        }
+
+                        override fun onCancelled(databaseError: DatabaseError) {
+                            Toast.makeText(context, databaseError.message, Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                    })
+                }
+                progressDialog.dismiss()
+            }
+            .addOnFailureListener {
+                // If the image upload fails, display an error message
+                Toast.makeText(context, "Image upload failed: ${it.message}", Toast.LENGTH_SHORT)
+                    .show()
+                progressDialog.dismiss()
+            }
+    }
+
+    private fun saveChangesToDatabase() {
+        // Display a success message and exit the fragment
+        Toast.makeText(context, "Changes saved successfully!", Toast.LENGTH_SHORT).show()
+        requireActivity().supportFragmentManager.popBackStack()
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AdminEditCus.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AdminEditCus().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        const val IMAGE_PICK_REQUEST_CODE = 100
     }
 }
