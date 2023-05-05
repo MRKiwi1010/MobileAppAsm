506990a9ef8be5be0d48e4ee5b11d63ba788f519
Ang Kang Wei <angkangwei4402@gmail.com>
2023-05-05 19:56:20 +0800

    newFile

--- app/src/main/java/com/example/mobileappasm/AdminDonationHistory.kt
+++ app/src/main/java/com/example/mobileappasm/AdminDonationHistory.kt
@@ -3,15 +3,27 @@ package com.example.mobileappasm
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil.setContentView
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mobileappasm.Adapter.CustomerAdapter
 import com.example.mobileappasm.Adapter.DonationAdapter
 import com.example.mobileappasm.databinding.FragmentAdminCustomerListBinding
 import com.example.mobileappasm.databinding.FragmentAdminDonationHistoryBinding
+import com.example.mobileappasm.ui.login.adminViewModel
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.database.*
 
 private const val ARG_PARAM1 = "param1"
@@ -29,6 +41,11 @@ class AdminDonationHistory : Fragment() {
     private lateinit var database: FirebaseDatabase
     private lateinit var donationRef: DatabaseReference
 
+    //drawer
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navView: NavigationView
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -41,12 +58,105 @@ class AdminDonationHistory : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentAdminDonationHistoryBinding.inflate(inflater, container, false)
-        recyclerView = binding.donationRecyclerView
+        //drawer
+        val rootView = inflater.inflate(R.layout.fragment_admin_donation_history, container, false)
+        drawerLayout = rootView.findViewById(R.id.drawerLayout)
+        navView = rootView.findViewById(R.id.navigationView) // initialize the navView property
+        //        navView.inflateMenu(R.menu.navigation_admin_drawer)
+
+        // enable the navigation drawer
+        setHasOptionsMenu(true)
+
+        // get the NavController for this fragment
+        val navController = this.findNavController()
+
+        // set up the ActionBar and the NavigationView with the NavController
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.adminDonationHistory), drawerLayout)
+        NavigationUI.setupActionBarWithNavController(
+            requireActivity() as AppCompatActivity,
+            navController,
+            appBarConfiguration
+        )
+
+        NavigationUI.setupWithNavController(navView, navController)
+
+        // handle navigation item clicks
+        navView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.adminProfile -> {
+                    // Handle click for adminProfile item
+                    view?.findNavController()?.navigate(R.id.adminProfile)
+                    true
+                }
+                R.id.adminViewAdminList -> {
+                    // Handle click for adminViewAdminList item
+                    view?.findNavController()?.navigate(R.id.adminViewAdminList)
+                    true
+                }
+                R.id.adminChildList -> {
+                    // Handle click for adminChildList item
+                    view?.findNavController()?.navigate(R.id.adminChildList)
+                    true
+                }
+                R.id.adminCustomerList -> {
+                    // Handle click for adminCustomerList item
+                    view?.findNavController()?.navigate(R.id.adminCustomerList)
+                    true
+                }
+                R.id.adminDonationHistory -> {
+                    // Handle click for adminDonationHistory item
+                    view?.findNavController()?.navigate(R.id.adminDonationHistory)
+                    true
+                }
+                R.id.adminLogout -> {
+                    AlertDialog.Builder(requireContext())
+                        .setTitle("Logout")
+                        .setMessage("Are you sure you want to logout?")
+                        .setPositiveButton("Yes") { dialog, which ->
+                            // Implement your logout logic here
+                            // For example, you could clear the user's session data and navigate them to the login screen.
+                            // You can use the following code to navigate to the login screen:
+                            val viewModel = ViewModelProvider(requireActivity())[adminViewModel::class.java]
+                            viewModel.username = ""
+                            view?.findNavController()?.navigate(R.id.AAHomePage)
+
+                            // Disable the drawer toggle
+                            val actionBar = (activity as AppCompatActivity?)?.supportActionBar
+                            actionBar?.setDisplayHomeAsUpEnabled(false)
+                            drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+
+                            // Clear the navigation drawer and remove the hamburger button
+                            navView.menu.clear()
+                            actionBar?.setHomeAsUpIndicator(null)
+                        }
+                        .setNegativeButton("No", null)
+                        .show()
+                    true
+                }
+                else -> false
+            }
+        }
+
+
+
+//        binding = FragmentAdminDonationHistoryBinding.inflate(inflater, container, false)
+        recyclerView = rootView.findViewById(R.id.donationRecyclerView)
         recyclerView.layoutManager = LinearLayoutManager(context)
         donationAdapter = DonationAdapter(requireContext())
         recyclerView.adapter = donationAdapter
-        return binding.root
+
+        return rootView
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                // open the navigation drawer when the hamburger icon is clicked
+                drawerLayout?.openDrawer(GravityCompat.START)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
