20e4cc42153d6ee6858b4574dc4a6ed4a82c16ba
Andy Ho <hoandy0507@gmail.com>
2023-05-05 19:47:24 +0800

    gfg

--- app/src/main/java/com/example/mobileappasm/AdminViewDonationDetails.kt
+++ app/src/main/java/com/example/mobileappasm/AdminViewDonationDetails.kt
@@ -1,59 +1,54 @@
 package com.example.mobileappasm
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.example.mobileappasm.databinding.FragmentAdminViewDonationDetailsBinding
+import com.google.firebase.database.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AdminViewDonationDetails.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AdminViewDonationDetails : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentAdminViewDonationDetailsBinding
+    private lateinit var firebaseDatabase: FirebaseDatabase
+    private lateinit var username: String
+    private lateinit var donation: Donation
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_view_donation_details, container, false)
+        binding = FragmentAdminViewDonationDetailsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AdminViewDonationDetails.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AdminViewDonationDetails().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        firebaseDatabase = FirebaseDatabase.getInstance()
+        username = arguments?.getString("username") ?: ""
+        fetchCustomerData()
+    }
+
+    private fun fetchCustomerData() {
+        val databaseReference = firebaseDatabase.getReference("payment")
+        val query = databaseReference.orderByChild("username").equalTo(username)
+        query.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    donation = dataSnapshot.children.first().getValue(Donation::class.java)!!
+                    binding.donationUsernameTextView.setText(donation.username)
+                    binding.donationChildNameTextView.setText(donation.childName)
+                    binding.donationAmountTextView.setText(donation.amount.toString())
+                    binding.donationDateTextView.setText(donation.date)
+                    binding.donationTimeTextView.setText(donation.time)
+                    binding.donationBankTypeTextView.setText(donation.bankType)
                 }
             }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Toast.makeText(context, databaseError.message, Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
